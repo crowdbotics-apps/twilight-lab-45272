df92feb0764fea60b374d8e9af3557b0888e105e
Crowdbotics <team@crowdbotics.com>
2024-01-04 15:38:54 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "twilight-lab-45272",
+  url: "https://twilight-lab-45272.botics.co",
+  api: "https://twilight-lab-45272.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
